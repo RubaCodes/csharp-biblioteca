a78b99ac164e3c0b82a45fb693523514d6c8b7b1
RubaCodes <rubacodes@gmail.com>
2022-09-20 14:36:31 +0100

    aggiunta classe user iniziale

--- Product.cs
+++ Product.cs
@@ -3,10 +3,9 @@ public class Product {
     public string SerialCode { get; set; }
     public string Title { get; set; }
     public int Year { get; set; }
-    public string Sector { get; set; }
+    public string Category { get; set; }
     public int Shelf { get; set; }
     public string AuthorFirstName { get; set; }
     public string AuthorLastName {get; set; }
-    // + durata movie 
-    //+ numero pagine libri
+    
 }
---
+++ User.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class User {
+
+public string LastName { get; set; }
+public string FirstName { get; set; }
+public string Email { get; set; }
+public string Password { get; set; }
+public double PhoneNumber { get; set; }
+}
