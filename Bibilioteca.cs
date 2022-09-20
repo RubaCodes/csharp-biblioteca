0fb1bb8b9b4ff6cf3bbcdaa2b9e8194b11986285
RubaCodes <rubacodes@gmail.com>
2022-09-20 14:48:15 +0100

    prima realizzazione biblioteca

---
+++ Bibilioteca.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Bibilioteca {
+    //lista utenti registrati
+    List<User> RegisterdUsers = new List<User>();
+    //aggiungi e rimuove utente dalla lista
+    public void AddNewUser(User utente) {
+        RegisterdUsers.Add(utente);
+    }
+    public void RemoveUser(User utente)
+    {
+        RegisterdUsers.Remove(utente);
+    }
+}
--- User.cs
+++ User.cs
@@ -1,9 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-public class User {
+﻿public class User {
+    public string LastName { get; set; }
+    public string FirstName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public double PhoneNumber { get; set; }
+    public bool Status { get; set; }
+    //costruttore
 
-public string LastName { get; set; }
-public string FirstName { get; set; }
-public string Email { get; set; }
-public string Password { get; set; }
-public double PhoneNumber { get; set; }
+    public User(string lastname, string firstname, string email, string password, double phoneNumber) {
+        LastName = lastname;
+        FirstName = firstname;  
+        Email = email;  
+        Password = password;
+        PhoneNumber = phoneNumber;  
+    }
 }
